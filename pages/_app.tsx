8437feda3aeeb26dd46e579309e4ada898161f3a
lhk3337 <lhk3337@naver.com>
2022-08-19 18:39:35 +0900

    [FEAT] tailwindcss 설치 및 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
+import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: any) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <SWRConfig
       value={{
-        fetcher: (url: string) => fetch(url).then((response) => response.json())
+        fetcher: (url: string) => fetch(url).then((response) => response.json()),
       }}
     >
-      <Component {...pageProps} />
+      <div className="w-full max-w-3xl mx-auto my-12">
+        <Component {...pageProps} />
+      </div>
     </SWRConfig>
   );
 }
